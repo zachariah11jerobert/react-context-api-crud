cf1c21a763a2681ff9b768c02954c30ca6364496
unknown <zachariah11jerobert@gmail.com>
2021-08-28 15:27:44 +0530

    done userlist component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,28 @@
+import {Link} from 'react-router-dom';
+import {
+    ListGroup,
+    ListGroupItem,
+    Button
+} from 'reactstrap';
+
 export const UserList = () => {
     return (
-        <div>
-            <h1>User List</h1>
-        </div>
+       <ListGroup className="mt-4">
+           <ListGroupItem className="d-flex">
+            <strong>User One</strong>
+            <div className="ml-auto">
+                <Link className="btn btn-warning mr-1" to="/edit/1">Edit</Link>
+                <Button color="danger">Delete</Button>
+            </div>
+           </ListGroupItem>
+
+           <ListGroupItem className="d-flex">
+            <strong>User One</strong>
+            <div className="ml-auto">
+                <Link className="btn btn-warning mr-1" to="/edit/1">Edit</Link>
+                <Button color="danger">Delete</Button>
+            </div>
+           </ListGroupItem>
+       </ListGroup>
     )
 }
