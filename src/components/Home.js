5d6f88b345b50594606466b55b9b48799c5480a6
unknown <zachariah11jerobert@gmail.com>
2021-08-28 15:20:04 +0530

    done header component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Heading } from './Heading'
+import { UserList } from './UserList'
 
 export const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <Heading/>
+            <UserList/>
+        </>
     )
 }
---
+++ src/components/UserList.js
@@ -0,0 +1,7 @@
+export const UserList = () => {
+    return (
+        <div>
+            <h1>User List</h1>
+        </div>
+    )
+}
