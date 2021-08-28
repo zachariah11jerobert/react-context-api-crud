02f1fc99a33b54d947c5c1cf621e44942c62da8b
unknown <zachariah11jerobert@gmail.com>
2021-08-28 19:19:14 +0530

    no user added when array is empty ,added unique key in userlist, finished project

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,21 +5,28 @@ import { Link } from "react-router-dom";
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 export const UserList = () => {
-    const {users,removeUser}=useContext(GlobalContext);
+  const { users, removeUser } = useContext(GlobalContext);
   return (
     <ListGroup className="mt-4">
-        {users.map(user=>(
-            <ListGroupItem className="d-flex">
-            <strong>{user.name}</strong>
-            <div className="ml-auto">
-              <Link className="btn btn-warning mr-1" to={`/edit/${user.id}`}>
-                Edit
-              </Link>
-              <Button onClick={()=>removeUser(user.id)} color="danger">Delete</Button>
-            </div>
-          </ListGroupItem>
-        ))}
-      
+      {users.length > 0 ? (
+        <>
+          {users.map((user) => (
+            <ListGroupItem className="d-flex" key={user.id}>
+              <strong>{user.name}</strong>
+              <div className="ml-auto">
+                <Link className="btn btn-warning mr-1" to={`/edit/${user.id}`}>
+                  Edit
+                </Link>
+                <Button onClick={() => removeUser(user.id)} color="danger">
+                  Delete
+                </Button>
+              </div>
+            </ListGroupItem>
+          ))}
+        </>
+      ) : (
+        <h1 className="text-center">No User</h1>
+      )}
     </ListGroup>
   );
 };
