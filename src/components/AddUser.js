6e8120bdfd96c9ce582351f629f61ffcd71786f4
unknown <zachariah11jerobert@gmail.com>
2021-08-28 18:11:28 +0530

    done add user part

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,25 +1,36 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import { Link, useHistory } from "react-router-dom";
+import { v4 as uuid } from "uuid";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export const AddUser = () => {
+  const [name, setName] = useState("");
   const { addUser } = useContext(GlobalContext);
   const history = useHistory();
 
   const onSubmit = () => {
     const newUser = {
-      id: 4,
-      name: "User Four",
+      id: uuid(),
+      name,
     };
     addUser(newUser);
     history.push("/");
+  };
+
+  const onChange = (e) => {
+    setName(e.target.value);
   };
   return (
     <Form onSubmit={onSubmit}>
       <FormGroup>
         <Label>Name</Label>
-        <Input type="text" placeholder="Enter Name"></Input>
+        <Input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={onChange}
+        ></Input>
       </FormGroup>
       <Button type="submit">Submit</Button>
       <Link to="/" className="btn btn-danger ml-2">
